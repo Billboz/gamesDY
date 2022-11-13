cab941caae011aa20e33bb41c5aeb4b265e113c4
billboz <billboz1@gmail.com>
2022-11-13 12:54:54 -0500

    Co-authored-by: Russell Baker <threetriangles@users.noreply.github.com>

--- lib/wordle.ex
+++ lib/wordle.ex
@@ -1,44 +1,91 @@
 defmodule Wordle do
-  def feedback(answer, guess) when answer == guess do
-    [:green, :green, :green, :green, :green]
+  @moduledoc """
+  Wordle Module can check how many exact matches, same position and letter,
+  occurrences, same letter, or no matches are between a word (``answer``)
+  and another word (``guess``).
+
+  - Exact matches => ``:green``
+  - Matches/Occurrences => ``:yellow``
+  - No matches => ``:gray``
+
+  Prioritizing exact matches, and then, prioritizing first appearances.
+
+  Both words have to contains same length and use only UTF-8 characters.
+  """
+
+  defp check_matches(guess, answer) do
+    for {g, a} <- Enum.zip(guess, answer), do: if(g == a, do: :green, else: g)
   end
 
-  def feedback(answer, guess) do
-    initial_game_state_list =
-      initial_filter(answer, guess) |> IO.inspect(label: "INITIAL_GAME_STATE")
+  defp check_occurrences(guess, answer) do
+    occurrences = for {g, a} <- Enum.zip(guess, answer), !is_atom(g), do: a
 
-    answer_list = String.codepoints(answer)
+    {_, result} =
+      Enum.reduce(
+        guess,
+        {occurrences, []},
+        fn guess_letter, {occurrences, result} ->
+          cond do
+            Enum.member?(occurrences, guess_letter) ->
+              {occurrences -- [guess_letter], [:yellow | result]}
 
-    zipped_game(answer_list, initial_game_state_list)
+            true ->
+              {occurrences, [guess_letter | result]}
+          end
+        end
+      )
+
+    result |> Enum.reverse()
   end
 
-  defp zipped_game(answer_list, initial_game_state) do
-    zipped_game = Enum.zip(answer_list, initial_game_state)
-
-    Enum.reduce(zipped_game, [], fn {a, g}, acc ->
-      cond do
-        a == g ->
-          acc ++ [:green]
-
-        g == :grey ->
-          acc ++ [:grey]
-
-        a != g ->
-          acc ++ [:yellow]
-      end
-    end)
+  defp no_matches(guess) do
+    Enum.map(guess, fn x -> unless is_atom(x), do: :gray, else: x end)
   end
 
-  defp initial_filter(answer, guess) do
-    answer = String.codepoints(answer)
-    guess = String.codepoints(guess)
+  @doc """
+  Compare an answer (1st arg) with a guess (2nd arg).
+  Returning a list where exact matches are represented as ``[:green]``,
+  occurrences are represented as ``[:yellow]`` (prioritizing first appearances)
+  and no matches are represented as ``[:gray]``.
 
-    Enum.map(guess, fn player_guess ->
-      if player_guess in answer do
-        player_guess
-      else
-        :grey
-      end
-    end)
+  Return a ``error`` if answer and gray are not binary with same length.
+
+  ## Examples
+      iex> Wordle.feedback("foo", "off")
+      [:yellow, :yellow, :gray ]
+
+      iex> Wordle.feedback("green", "grnne")
+      [:green, :green, :yellow, :gray, :yellow ]
+
+      iex> Wordle.feedback("greed", "green")
+      [:green, :green, :green, :green, :gray ]
+
+      iex> Wordle.feedback("greed", 1)
+      {:error, "Both arguments have to be strings."}
+
+      iex> Wordle.feedback("greed", "aaaaaaaaaaaaaaaaaa")
+      {:error, "Both arguments have to be strings with same length"}
+  """
+
+  @spec feedback(binary, binary) :: {:ok, list} | {:error, any}
+  def feedback(answer, guess) when is_binary(answer) and is_binary(guess) do
+    answer_list = answer |> String.downcase() |> String.to_charlist()
+    guess_list = guess |> String.downcase() |> String.to_charlist()
+
+    if String.length(answer) == String.length(guess) do
+      result =
+        guess_list
+        |> check_matches(answer_list)
+        |> check_occurrences(answer_list)
+        |> no_matches()
+
+      {:ok, result}
+    else
+      {:error, "Both arguments have to be strings with same length"}
+    end
+  end
+
+  def feedback(_answer, _guess) do
+    {:error, "Both arguments have to be strings."}
   end
 end
--- test/wordle_test.exs
+++ test/wordle_test.exs
@@ -11,31 +11,54 @@ defmodule WordleTest do
     assert Wordle.feedback("aaaaa", "bbbbb") == [:grey, :grey, :grey, :grey, :grey]
   end
 
+  # @tag :skip
   test "feedback/2 all character are wrong position / yellow" do
     assert Wordle.feedback("abdce", "edcba") == [:yellow, :yellow, :yellow, :yellow, :yellow]
   end
 
+  @tag :skip
   test "feedback/2 one character is incorrect / grey" do
     assert Wordle.feedback("aaaaa", "aaaab") == [:green, :green, :green, :green, :grey]
   end
 
+  @tag :skip
   test "feedback/2 mixture of green and yellow" do
     assert Wordle.feedback("abaaa", "baaaa") == [:yellow, :yellow, :green, :green, :green]
   end
 
+  @tag :skip
   test "feedback/2 mixture of grey and yellow" do
     assert Wordle.feedback("zzdef", "bafde") == [:grey, :grey, :yellow, :yellow, :yellow]
   end
 
+  @tag :skip
   test "feedback/2 mixture of grey and green" do
     assert Wordle.feedback("zzdef", "badef") == [:grey, :grey, :green, :green, :green]
   end
 
+  @tag :skip
   test "feedback/2 mixture of grey, green, and yellow" do
     assert Wordle.feedback("zzdef", "badfe") == [:grey, :grey, :green, :yellow, :yellow]
   end
 
+  @tag :skip
   test "feedback/2 valid but excess characters" do
-    assert Wordle.feedback("aaabb", "xaaaa") == [:yellow, :green, :green, :yellow, :grey]
+    assert Wordle.feedback("aaabb", "xaaaa") == [:grey, :green, :green, :yellow, :grey]
+  end
+
+  @tag :benchmark
+  test "feedback/2 Benchmark" do
+    Benchee.run(%{
+      "wordle green" => fn -> Wordle.feedback("hello", "hello") end,
+      "wordle one yellow" => fn -> Wordle.feedback("hello", "abcde") end,
+      "wordle gray" => fn -> Wordle.feedback("hello", "abcdz") end,
+      "wordle yellow" => fn -> Wordle.feedback("hello", "olehl") end,
+      # "wordle long string" => fn ->
+      #   Wordle.feedback(
+      #     List.to_string(for i <- 1..100, do: String.to_charlist(?a..?z)),
+      #     List.to_string(for i <- 1..100, do: for(i <- ?a..?z, do: i))
+        )
+      end
+    })
   end
 end
